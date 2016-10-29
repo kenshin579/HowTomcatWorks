820cb008e2726d486bb82410baba0e41d2db984d
Administrator <user@example.com>
2016-10-29 14:27:12 +0900

    chap2 파일 추가

---
+++ src/main/java/com/brainysoftware/pyrmont/chap2/StaticResourceProcessor.java
@@ -0,0 +1,14 @@
+package com.brainysoftware.pyrmont.chap2;
+
+import java.io.IOException;
+
+public class StaticResourceProcessor {
+
+    public void process(Request request, Response response) {
+        try {
+            response.sendStaticResource();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
